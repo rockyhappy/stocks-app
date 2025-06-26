65d3f6eb40adbdd07498af25392532864dadba06
rockyhappy <rachit22153019@akgec.ac.in>
2025-06-26 17:42:13 +0530

    Small changes

--- app/src/main/java/com/devrachit/groww/MainActivity.kt
+++ app/src/main/java/com/devrachit/groww/MainActivity.kt
@@ -109,18 +109,3 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GrowwTheme {
-        Greeting("Android")
-    }
-}
